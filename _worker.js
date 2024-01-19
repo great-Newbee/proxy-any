2cb74ab550cd5f05a5e8dafbe4a8fce1b3aca671
hukaiKK <64007287+great-Newbee@users.noreply.github.com>
2024-01-19 22:35:04 +0800

    Update _worker.js
    修改

--- _worker.js
+++ _worker.js
@@ -1,34 +1,37 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+
+export default {
+  async fetch(request, env) {
+      const NewResponse = await handleRequest(request)
+      return NewResponse
+  },
+
+};
+
+
 
 async function handleRequest(request) {
   const url = new URL(request.url);
-
-  // 新增：检查用户是否直接访问代理地址
-  // if (url.pathname === '/' || url.pathname === '/proxy/') {
-  //   return createLandingPage();
-  // }
-
-  // const actualUrlStr = url.pathname.replace("/proxy/","") + url.search + url.hash
-const actualUrlStr =  url.search + url.hash
-  const actualUrl = new URL(actualUrlStr)
-
-  const modifiedRequest = new Request(actualUrl, {
+    // 新增：检查用户是否直接访问代理地址
+  if (url.pathname === '/proxy/') {
+    return createLandingPage();
+  }
+  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
+  // 代理网站
+  const actualUrlStr =  url.search + url.hash
+  url.host = actualUrlStr.replace(/^https?:\/\//, '');
+  const modifiedRequest = new Request(url.toString(), {
     headers: request.headers,
     method: request.method,
     body: request.body,
     redirect: 'follow'
   });
-
   const response = await fetch(modifiedRequest);
   const modifiedResponse = new Response(response.body, response);
-
   // 添加允许跨域访问的响应头
-  modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
-
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
   return modifiedResponse;
 }
+
 
 // 新增：创建引导页面
 function createLandingPage() {
